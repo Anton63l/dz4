efd6d7e81962fbed565ac78fbaa4e065a01192de
Anton <anton@mail.ru>
2022-09-14 11:27:03 +0400

    Финал

--- Program.cs
+++ Program.cs
@@ -22,46 +22,19 @@
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 
-// System.Console.WriteLine("Введите число: ");
+// System.Console.Write("Введите число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
 
 // int SumDigit(int n){
 //     int result = 0;
-//     int newnum = 0;
-
-//     for (int i = 0; i <Convert.ToString(n).Length; i++)
-//     {
-//        result = result +  n % 10;
-          
-
-
+//     while(n > 0){
+//         result = result + n % 10;
+//         n = n / 10;
 //     }
-
+//     return result;
 // }
 
-// System.Console.WriteLine(SumDigit(num));
-
-
-// nt SumNumber(int numberN){
-    
-//     int counter = Convert.ToString(numberN).Length;
-//     int advance = 0;
-//     int result = 0;
-
-//     for (int i = 0; i < counter; i++){
-//       advance = numberN - numberN % 10;
-//       result = result + (numberN - advance);
-//       numberN = numberN / 10;
-//     }
-//    return result;
-//   }
-
-
-//  int num = Convert.ToInt32(Console.ReadLine());
-// int numm = 0;
-// num %=10;
-// numm = num - num / 10;
-// System.Console.WriteLine(numm);
+// Console.WriteLine($"Cумма всех цифр в чисое равна {SumDigit(num)}");
 
 
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
